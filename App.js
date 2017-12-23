cfb2178a2d1adb8043e4f916b4bf92027619d2e1
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-23 04:36:24 -0600

    Initial content

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import https from 'https';
+import fs from 'fs'
+
+const pl = PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ
+const key = fs.readFileSync('youtube-key')
+
+const options = {
+  hostname: 'googleapis.com/youtube/v3/playlistItems',
+  port: 443,
+  path: '?part=id%2Csnippet&playlistId= ' + pl + '&key=' + key,
+  method: 'GET'
+};
 
 export default class App extends React.Component {
   render() {
+    let playlist
+    const req = https.request(options, (res) => {
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
+
+      res.on('data', (d) => {
+        playlist = d
+      });
+    });
+
+    req.on('error', (e) => {
+      console.error(e);
+    });
+    req.end();
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{JSON.stringify(playlist)}</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
