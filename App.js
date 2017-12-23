ff28f1d394cdc9179c655a207e7bc996eaf09698
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-23 05:38:15 -0600

    About that.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     let playlist
-    fetch('https://googleapis.com/youtube/v3/playlistItems', {
+    playlist = fetch('https://googleapis.com/youtube/v3/playlistItems', {
       method: 'GET',
       headers: {
         Accept: 'application/json',
