6ccae3e575fd9676515316ce1fe1d4dfb6eba575
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-26 16:53:30 -0600

    Adding fetch()

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import yt from 'react-native-youtube'
 
 export default class App extends React.Component {
   render() {
     let playlist = ['oh no']
-    
+
+    playlist = fetch('https://googleapis.com/youtube/v3/playlistItems', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            part: 'id,snippet',
+            playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
+            key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
+        }),
+    });
+
     return (
       <View style={styles.container}>
         <Text>{JSON.stringify(playlist)}</Text>
