705ca9fc28747a42f9603ef87e212fc58bc2ad81
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-27 01:06:46 -0600

    I don't find the fetch() documentation completely informative.

--- App.js
+++ App.js
@@ -13,6 +13,21 @@ export default class App extends React.Component {
     //   key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
     // });
 
+    let c = {
+        part: 'id,snippet',
+        playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
+        key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
+    }
+
+    playlist = fetch('https://googleapis.com/youtube/v3/playlistItems?part=' + c.part + '&playlistId=' + c.playlistId + '&key=' + c.key, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    });
+
+
     return (
       <View style={styles.container}>
         <Text>{JSON.stringify(playlist)}</Text>
