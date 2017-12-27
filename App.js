0d07281b8f83b7522e72ee63b16caea570f87fa2
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-27 01:30:13 -0600

    Oh my.

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import yt from 'react-native-youtube'
 // import buildApiRequest from './google-api'
 
 export default class App extends React.Component {
-  render() {
-    let playlist = ['oh no']
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
 
-    // playlist = buildApiRequest('GET', 'youtube/v3/playlistItems', {
-    //   part: 'id,snippet',
-    //   playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
-    //   key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
-    // });
-
+  onComponentDidMount() {
     let c = {
         part: 'id,snippet',
         playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
@@ -26,21 +25,34 @@ export default class App extends React.Component {
             'Content-Type': 'application/json',
         }
     }).then( response => response.json()).then( playlist => {
-      return (
-        <View style={styles.container}>
-          <Text>{JSON.stringify(playlist)}</Text>
-        </View>
-      )
+      this.setState({
+        isLoading: false,
+        content: JSON.stringify(playlist)
+      })
     }).catch( error => {
-      return (
-        <View style={styles.container}>
-          <Text>The request didn't work out.</Text>
-        </View>
-      )
+      this.setState({
+        isLoading: false,
+        content: error
+      })
     })
+  }
 
+  render() {
+    let playlist = ['oh no']
 
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
 
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.content}</Text>
+      </View>
+    )
   }
 }
 
