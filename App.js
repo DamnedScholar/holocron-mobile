e0c0d7783912f1a287a68d4adf1a7388de8f3717
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-26 23:27:29 -0600

    Checking to see if the error is because of api.js

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import yt from 'react-native-youtube'
-import buildApiRequest from './google-api'
+// import buildApiRequest from './google-api'
 
 export default class App extends React.Component {
   render() {
     let playlist = ['oh no']
 
-    playlist = buildApiRequest('GET', 'youtube/v3/playlistItems', {
-      part: 'id,snippet',
-      playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
-      key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
-    });
+    // playlist = buildApiRequest('GET', 'youtube/v3/playlistItems', {
+    //   part: 'id,snippet',
+    //   playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
+    //   key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
+    // });
 
     return (
       <View style={styles.container}>
