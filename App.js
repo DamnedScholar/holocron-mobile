6c5c41526aa27bbe628a6e2e5aac245b99275044
DamnedScholar <damnedscholar@gmail.com>
2017-12-28 00:30:44 +0000

    Last commit before I eject, I think.

--- App.js
+++ App.js
@@ -1,16 +1,73 @@
 import React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import yt from 'react-native-youtube'
+import {
+  ActivityIndicator, FlatList, ScrollView, StyleSheet, Text, TouchableNativeFeedback, View
+} from 'react-native';
+import YouTube from 'react-native-youtube'
 // import buildApiRequest from './google-api'
+
+class VideoView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>We're in the video view.</Text>
+        <YouTube
+          videoId={this.props.activeVideo}
+          play={true}
+          fullscreen={true}
+          loop={true}
+
+          onReady={e => this.setState({ isReady: true })}
+          onChangeState={e => this.setState({ status: e.state })}
+          onChangeQuality={e => this.setState({ quality: e.quality })}
+          onError={e => this.setState({ error: e.error })}
+
+          style={{ alignSelf: 'stretch', height: 300 }}
+        />
+      </View>
+    )
+  }
+}
+
+class TitleView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>We're in the title view.</Text>
+        <Text>Some data: {JSON.stringify(Object.keys(this.props.response.items[0].snippet))}</Text>
+        <FlatList
+          data={this.props.response.items}
+          keyExtractor={item => item.snippet.resourceId.videoId}
+          renderItem={({item}) => <TouchableNativeFeedback
+            onPress={() => {
+              this.props.navigate(item.snippet.resourceId.videoId)
+            }}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+              <Text>{item.snippet.title + '- https://youtube.com/watch?v=' + item.snippet.resourceId.videoId}</Text></TouchableNativeFeedback>}
+        />
+      </View>
+    )
+  }
+}
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      result: "",
-      content: ""
+      response: "",
+      view: TitleView,
+      activeVideo: null
     }
+
+    this.navigate = this.navigate.bind(this)
+  }
+
+  async componentWillMount() {
+      await Expo.Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      });
   }
 
   componentDidMount() {
@@ -20,7 +77,8 @@ export default class App extends React.Component {
         key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
     }
 
-    return fetch('https://googleapis.com/youtube/v3/playlistItems?part=' + c.part + '&playlistId=' + c.playlistId + '&key=' + c.key, {
+    // Note to future self: Google API calls have to be aimed at `www.googleapis.com`, not `googleapis.com`.
+    return fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=' + c.part + '&playlistId=' + c.playlistId + '&key=' + c.key, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -29,17 +87,28 @@ export default class App extends React.Component {
     }).then( response => response.json()).then( playlist => {
       this.setState({
         isLoading: false,
-        result: "Success!",
-        content: JSON.stringify(playlist)
+        response: playlist
       })
     }).catch( error => {
       console.error(error)
     })
   }
 
-  render() {
-    let playlist = ['oh no']
+  navigate(id) {
+    if (id === "title") {
+      this.setState({
+        view: TitleView,
+      })
+    }
+    else {
+      this.setState({
+        view: VideoView,
+        activeVideo: id
+      })
+    }
+  }
 
+  render() {
     if (this.state.isLoading) {
       return (
         <View style={{flex: 1, paddingTop: 20}}>
@@ -50,8 +119,7 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Text>{this.state.result}</Text>
-        <Text>{this.state.content}</Text>
+        <this.state.view {...this.state} navigate={this.navigate} />
       </View>
     )
   }
