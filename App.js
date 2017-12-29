c57c9e96c935552e161262649aea1f47241b98e7
DamnedScholar <stickfigureonfire@gmail.com>
2017-12-29 13:54:11 -0600

    Reworking things

--- App.js
+++ App.js
@@ -1,48 +1,24 @@
 import React from 'react';
 import {
-  ActivityIndicator, FlatList, ScrollView, StyleSheet, Text, TouchableNativeFeedback, View
+  ActivityIndicator, FlatList, NativeModules, ScrollView, StyleSheet, Text, TouchableNativeFeedback, View
 } from 'react-native';
-import YouTube from 'react-native-youtube'
-// import buildApiRequest from './google-api'
-
-class VideoView extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>We're in the video view.</Text>
-        <YouTube
-          videoId={this.props.activeVideo}
-          play={true}
-          fullscreen={true}
-          loop={true}
-
-          onReady={e => this.setState({ isReady: true })}
-          onChangeState={e => this.setState({ status: e.state })}
-          onChangeQuality={e => this.setState({ quality: e.quality })}
-          onError={e => this.setState({ error: e.error })}
-
-          style={{ alignSelf: 'stretch', height: 300 }}
-        />
-      </View>
-    )
-  }
-}
+import { Video } from 'expo'
+import { Houndify } from 'houndify'
 
 class TitleView extends React.Component {
   render() {
     return (
-      <View>
-        <Text>We're in the title view.</Text>
-        <Text>Some data: {JSON.stringify(Object.keys(this.props.response.items[0].snippet))}</Text>
+      <View style={{flex: 1, borderWidth: 1, borderColor: "black"}}>
+        <Text style={{flex: 1, borderWidth: 1, borderColor: "black"}}>This is the title view</Text>
         <FlatList
-          data={this.props.response.items}
-          keyExtractor={item => item.snippet.resourceId.videoId}
+          data={this.props.videos}
           renderItem={({item}) => <TouchableNativeFeedback
             onPress={() => {
-              this.props.navigate(item.snippet.resourceId.videoId)
+              this.props.navigate(item.key)
             }}
+            style={{flex: 1, borderWidth: 1, borderColor: "black"}}
             background={TouchableNativeFeedback.SelectableBackground()}>
-              <Text>{item.snippet.title + '- https://youtube.com/watch?v=' + item.snippet.resourceId.videoId}</Text></TouchableNativeFeedback>}
+              <Text>{item.name}</Text></TouchableNativeFeedback>}
         />
       </View>
     )
@@ -53,11 +29,44 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true,
+      isLoading: false,
       response: "",
-      view: TitleView,
+      view: "title",
       activeVideo: null
     }
+
+    this.videos = [
+      {
+        name: "Blue Spin",
+        key: "bluespin",
+        file: 'https://www.dropbox.com/s/syolzmu250crd3x/AR_BLUE_4_512kb.mp4'
+      },
+      {
+        name: "Rabbit Hole",
+        key: "rabbithole",
+        file: 'https://www.dropbox.com/s/xe6umzvakwo38hf/AR_COLOUR_1_512kb.mp4'
+      },
+      {
+        name: ".react",
+        key: "dotreact",
+        file: 'https://www.dropbox.com/s/uy3qj3gbls50apw/AR_GRAFX_GLO_1_512kb.mp4'
+      },
+      {
+        name: "The Three Spheres at the End of the Universe",
+        key: "threespheres",
+        file: 'https://www.dropbox.com/s/ztpskhush2kk4hf/AR_GRAFX_GLO_2_512kb.mp4'
+      },
+      {
+        name: "Fiery Knobs",
+        key: "knobs",
+        file: 'https://www.dropbox.com/s/ct3xs317eclmcl6/AR_GRAFX_KNOBS_512kb.mp4'
+      },
+      {
+        name: "Purple Tesseract",
+        key: "tesseract",
+        file: 'https://www.dropbox.com/s/knstjnle123oxey/AR_GRAFX_PURPLE_2_512kb.mp4'
+      }
+    ]
 
     this.navigate = this.navigate.bind(this)
   }
@@ -70,40 +79,18 @@ export default class App extends React.Component {
       });
   }
 
-  componentDidMount() {
-    let c = {
-        part: 'id,snippet',
-        playlistId: 'PLLCvyMTFX6UbTsPaBVEGvejUL4yZC1ExQ',
-        key: 'AIzaSyAeeVWvyfGbzZd5dBrkkAPe7IAUu6HcqRo'
-    }
-
-    // Note to future self: Google API calls have to be aimed at `www.googleapis.com`, not `googleapis.com`.
-    return fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=' + c.part + '&playlistId=' + c.playlistId + '&key=' + c.key, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        }
-    }).then( response => response.json()).then( playlist => {
+  navigate(key) {
+    if (key === "title") {
+      console.log("Activating the title view")
       this.setState({
-        isLoading: false,
-        response: playlist
-      })
-    }).catch( error => {
-      console.error(error)
-    })
-  }
-
-  navigate(id) {
-    if (id === "title") {
-      this.setState({
-        view: TitleView,
+        view: "title",
       })
     }
     else {
+      console.log("Video " + key + " selected.")
       this.setState({
-        view: VideoView,
-        activeVideo: id
+        view: "video",
+        activeVideo: key
       })
     }
   }
@@ -117,10 +104,58 @@ export default class App extends React.Component {
       );
     }
 
+    console.log("View: " + this.state.view)
+
+    let activeVideo
+    activeVideo = this.videos.filter(v => v.id === this.state.activeVideo)[0]
+    if (!activeVideo)
+      activeVideo = {file: null, name: null, key: null}
+
+    let titleViewStyle = {
+      flex: 1,
+      alignItems: 'center',
+      display: 'none',
+      justifyContent: 'center',
+      borderWidth: 1,
+      borderColor: "black"
+    }
+    let playerStyle = {
+      alignSelf: 'stretch',
+      height: 300
+    }
+
+    if (this.state.view != "title") {
+      titleViewStyle.width = titleViewStyle.height = 0
+      console.log("Hiding the title view")
+    }
+    else
+      playerStyle.display = "none"
+
+    const localStyles = StyleSheet.create({
+      titles: titleViewStyle, player: playerStyle})
+
     return (
-      <View style={styles.container}>
-        <this.state.view {...this.state} navigate={this.navigate} />
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.heading}>[App: Title view]</Text>
+        <TitleView style={localStyles.titles}
+          videos={this.videos} navigate={this.navigate}
+          {...this.state}
+        />
+        <Text style={styles.heading}>[App: Player view, {activeVideo.name}]</Text>
+        <Video
+          source={{ uri: activeVideo.file ? activeVideo.file : null }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{
+            flex: 1, width: 300, height: 300,
+            borderWidth: 1, borderColor: "black"
+          }}
+        />
+      </ScrollView>
     )
   }
 }
@@ -132,4 +167,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "black"
+  }
 });
